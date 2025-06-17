414edd49560eab3c11636ec1d269a54c625f9130
serverkanb <118963895+serverkanb@users.noreply.github.com>
2025-06-17 13:38:39 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-1IHFPICF;database=CoreProject;trusted_connection=true;multipleactiveresultsets=true;trustservercertificate=true");
+        }
+
+        public DbSet<Service> Services { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<TestimOnial> TestimOnials { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+
+    }
+}
